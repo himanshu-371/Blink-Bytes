3f32757c2f62a28f9d09b4fccc9d20afa1e9f4b1
himanshu-371 <himanshuchauhan371@gmail.com>
2024-03-30 20:50:31 +0530

    completed create a post page ui, added create a post api route, completed upload post image functionality, completed create post functionality, added posts section to the dashboard, created get posts api route, show user posts inside dashboard, addded show more functionality to the posts result of the dashboard, added delete post functionality to the dashboard, added update post functionality, created get users API route, show users to the admin dashboard, added delete user functionality to the admin user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,9 @@ import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
 
 function App() {
   return (
@@ -22,6 +25,11 @@ function App() {
 
           <Route element={<PrivateRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+
+          <Route element={<OnlyAdminPrivateRoute />}>
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/update-post/:postId" element={<UpdatePost />} />
           </Route>
 
           <Route path="/projects" element={<Projects />} />
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,28 +1,36 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import DashSidebar from "../components/DashSidebar";
-import DashProfile from "../components/DashProfile";
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar';
+import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
+import DashUsers from '../components/DashUsers';
+// import DashComments from '../components/DashComments';
+// import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
   const location = useLocation();
-  const [tab, setTab] = useState();
+  const [tab, setTab] = useState('');
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-
-    const tabFromUrl = urlParams.get("tab");
+    const tabFromUrl = urlParams.get('tab');
     if (tabFromUrl) {
       setTab(tabFromUrl);
     }
   }, [location.search]);
-
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
-      <div className="md:w-56 ">
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
         {/* Sidebar */}
-        <DashSidebar></DashSidebar>
+        <DashSidebar />
       </div>
       {/* profile... */}
-      {tab === "profile" && <DashProfile />}
+      {tab === 'profile' && <DashProfile />}
+      {/* posts... */}
+      {tab === 'posts' && <DashPosts />}
+      {/* users */}
+      {tab === 'users' && <DashUsers />}
+      {/* comments  */}
+      {/* dashboard comp */}
     </div>
   );
 }
